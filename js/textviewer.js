cb059cf4d3166ce9000405325467b2296e8cc1cd
Jamie Norrish <jamie@artefact.org.nz>
2014-03-26 10:04:35 +1300

    Added/updated full textviewer code and resources.

--- js/textviewer.js
+++ js/textviewer.js
@@ -4,14 +4,43 @@ function TextViewer(lowerBoundary) {
     var self = this;
 
     this.panels = [];
+    this.locationId = '';
     this.maxHeight = $(lowerBoundary).offset().top;
 
     $(window).bind('resize', function() {
-        self.onResize(); 
+        self.onResize();
     });
     $(window).bind('scroll', function() {
         self.onResize();
     });
+    this.switches = { };
+}
+
+/**
+ * Adds a new TextPanel to the TextViewer.
+ * @param contentUrl Base URL to get content.
+ * @param textSelect Select element that defines the content.
+ * @param box Element to display the text.
+ * @param syncSelector Name of the element used to sync the different panels.
+ */
+TextViewer.prototype.addPanel = function(contentUrl, textSelect, box, syncSelector) {
+    this.panels.push(new TextPanel(this, this.panels.length, contentUrl,
+                                   textSelect, box, syncSelector));
+}
+
+/**
+ * Returns the TextViewer maximum height.
+ */
+TextViewer.prototype.getMaxHeight = function() {
+    return this.maxHeight;
+}
+
+TextViewer.prototype.getSwitch = function(name) {
+    return this.switches[name];
+}
+
+TextViewer.prototype.setSwitch = function(name, value) {
+    this.switches[name] = value;
 }
 
 /**
@@ -23,67 +52,131 @@ TextViewer.prototype.onResize = function() {
     }
 }
 
-/**
- * Adds a new TextPanel to the TextViewer.
- * @param contentUrl Base URL to get content.
- * @param type Select element that defines the type of content.
- * @param box Element to display the text.
- */
-TextViewer.prototype.addPanel = function(contentUrl, type, box) {
-    this.panels.push(new TextPanel(this, this.panels.length, contentUrl, type, 
-                box));
+TextViewer.prototype.synchronisePanels = function(referencePanel) {
+    if (!this.getSwitch('sync')) {
+        return;
+    }
+
+    this.highlight(this.locationId, false, false);
+    this.locationId = referencePanel.getLocationId();
+    this.highlight(this.locationId, true, true);
+
+    for (i in this.panels) {
+        var panel = this.panels[i];
+        if (panel != referencePanel) {
+            panel.syncToLocationId(this.locationId);
+        }
+    }
+}
+
+TextViewer.prototype.highlight = function(locationId, active, bothPanes) {
+    if (!this.getSwitch('highlight')) {
+        return;
+    }
+
+    var elements = $('*[data-text-id = ' + locationId + ']');
+
+    if (!bothPanes || elements.length > 1) {
+        $.each(elements, function(i, element) {
+            var obj = $(element);
+
+            if (obj.children().length < 1) {
+                obj = obj.parent();
+            }
+
+            if (active) {
+                obj.addClass('highlight');
+            } else {
+                obj.removeClass('highlight');
+            }
+        });
+    }
 }
 
 /**
- * Returns the TextViewer maximum height.
+ * Updates the location.
  */
-TextViewer.prototype.getMaxHeight = function() {
-    return this.maxHeight;
+TextViewer.prototype.updateLocation = function() {
+    var hash = '';
+    for (panel in this.panels) {
+        if (panel > 0) {
+            hash += '/';
+        }
+        hash += this.panels[panel].getLocation();
+    }
+    location.hash = hash;
 }
 
 
 /**
  */
-function TextPanel(viewer, position, contentUrl, type, box) {
+function TextPanel(viewer, position, contentUrl, textSelect, box, syncSelector) {
     var self = this;
 
     this.viewer = viewer;
     this.position = position;
     this.contentUrl = contentUrl;
-    this.type = type;
+    this.textSelect = textSelect;
     this.box = box;
-
-    this.type.change(function() {
-        self.onTypeChange();
+    this.beingSynced = false;
+    this.locationId = '';
+    this.textSelect.change(function() {
+        self.onTextSelectChange();
     });
-
-    this.bindOnScroll();
+    this.bindOnScroll(true);
     this.onResize();
-    this.onTypeChange();
+    this.onTextSelectChange();
 }
 
 /**
- * TODO: This needs more functionality.
- * Handle type changes, loads new content into the panel according to the type.
- */
-TextPanel.prototype.onTypeChange = function() {
-    var self = this;
-
-    var data = $.ajax({
-        url: self.contentUrl + self.type.val() + '.content',
-        async: false,
-        type: 'GET',
-    });
-
-    this.box.html(data.responseText);
-}
-
-/**
- * TODO: Implement.
  * Bind scroll event.
  */
 TextPanel.prototype.bindOnScroll = function(bind) {
     var self = this;
+
+    if (this.onScroll == null) {
+        this.onScroll = function(event) {
+            self.onVisibleContentChange($(event.target));
+        }
+    }
+
+    if (bind) {
+        this.box.bind('scroll', this.onScroll);
+    } else {
+        this.box.unbind('scroll', this.onScroll);
+    }
+}
+
+TextPanel.prototype.getFirstVisibleElement = function(selector, windowHeight) {
+        // get_first_visible_element($('.viewer-text-box'), 'p')
+        var ret = null;
+        if (windowHeight == null) {
+            windowHeight = 40;
+        }
+        var windowTop = this.box.offset().top;
+        var windowBottom = windowTop + windowHeight;
+        var elements = this.box.find(selector).each(function() {
+            var top = $(this).offset().top;
+            if (top <= windowBottom) {
+                ret = $(this);
+            }
+            return (top < windowTop);
+        });
+        return ret;
+    }
+
+/**
+ * Returns the panel location.
+ */
+TextPanel.prototype.getLocation = function() {
+    return this.textSelect.val();
+}
+
+/**
+ * Returns the ID of the current place in the text.
+ */
+TextPanel.prototype.getLocationId = function() {
+    return this.locationId;
 }
 
 /**
@@ -101,44 +194,71 @@ TextPanel.prototype.onResize = function() {
     height = parseInt(height) + 'px';
 
     this.box.css('height', height);
-    this.onVisibleContentChange()
+    this.onVisibleContentChange();
 }
 
 /**
- * TODO: Implement.
+ * Handle type changes, loads new content into the panel according to the type.
+ */
+TextPanel.prototype.onTextSelectChange = function() {
+    var self = this;
+
+    if (this.textSelect.val() && this.textSelect.val().length > 0) {
+        this.box.html('<div class="spinner"> </div>');
+
+        $.ajax({
+            url: self.contentUrl + self.textSelect.val() + '/',
+            async: false,
+            type: 'GET',
+            success: function(data, textStatus, xhr) {
+                data = data.trim();
+                var toc = $(data).filter('#text-toc');
+                var content = $(data).filter('#text-content');
+
+                self.box.parent().find('.panel-tool-bar').find('#text-toc').remove();
+                self.box.parent().find('.panel-tool-bar').append(toc);
+                self.box.html(content);
+            }
+        });
+
+        this.viewer.updateLocation();
+    } else {
+        this.box.html('');
+        this.viewer.updateLocation();
+    }
+}
+
+/**
  * Handle changes to the box content.
  */
 TextPanel.prototype.onVisibleContentChange = function() {
-    
+    var element = this.getFirstVisibleElement('*[data-text-id]');
+    this.locationId = element ? element.attr('data-text-id') : '';
+    if (!this.beingSynced) {
+        this.viewer.synchronisePanels(this);
+    }
+    this.beingSynced = false;
 }
 
-/**
- * Returns the ID in the other displayed text that corresponds with
- * the place in the displayed text indicated by text_id.
- *
- * TODO: This presumably requires (in some setups at least) an
- * argument indicating which text text_id belongs to.
- */
-TextPanel.prototype.matchIds = function(text_id) {
-
+TextPanel.prototype.scrollBoxToElement = function(element) {
+    var ret = false;
+    if (element.offset()) {
+        var before = this.box.scrollTop();
+        this.box.scrollTop(element.offset().top - this.box.offset().top +
+                           before);
+        if (this.box.scrollTop() == before) {
+            this.beingSynced = false;
+        }
+        ret = true;
+    }
+    return ret;
 }
 
-/**
- * Returns the ID associated with label.
- *
- * This function allows for the user to enter a label and the viewer
- * to jump to that point in the text.
- */
-TextPanel.prototype.getIdFromLabel = function(label) {
-    return '';
+TextPanel.prototype.setLocationId = function(locationId) {
+    return this.scrollBoxToElement(this.box.find('*[data-text-id=' + locationId + ']').first());
 }
 
-/**
- * Returns the label string associated with id.
- *
- * This functions allows for the label of the currently viewed section
- * of a text (associated with id) to be displayed.
- */
-TextPanel.prototype.getLabelFromId = function(id) {
-    return '';
+TextPanel.prototype.syncToLocationId = function(locationId) {
+    this.beingSynced = true;
+    return this.setLocationId(locationId);
 }
